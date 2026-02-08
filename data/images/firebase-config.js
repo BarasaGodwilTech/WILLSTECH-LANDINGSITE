87742f77c47fcacb04c10ff71079535f0702946e
BarasaGodwilTech <barasagodwil@gmail.com>
2026-02-08 20:53:26 +0300

    Add files via upload

---
+++ data/images/firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+// This file contains Firebase configuration that will be used by both index.html and admin.html
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDYdrpyNEPMFTYrKft-xs5TfKZkKlcbXYE",
+  authDomain: "ezee-money-agent-registration.firebaseapp.com",
+  projectId: "ezee-money-agent-registration",
+  storageBucket: "ezee-money-agent-registration.firebasestorage.app",
+  messagingSenderId: "391153750588",
+  appId: "1:391153750588:web:c9355b62cab43080c3e939",
+  measurementId: "G-YF5H7HY7E7"
+};
